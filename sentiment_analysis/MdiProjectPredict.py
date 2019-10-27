0c6a44dbe03303e8566ace3b0f95addc8fb06bb2
Santosh-Sah <santosh1981.gupta@gmail.com>
2019-10-27 12:56:01 +0530

    Update MdiProjectPredict.py

--- sentiment_analysis/MdiProjectPredict.py
+++ sentiment_analysis/MdiProjectPredict.py
@@ -241,5 +241,5 @@ def sentimentsAnalysisBasedOnTweetTopicUsingLogisticRegression():
     return response
         
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=5000)
     #sentimentsAnalysisSingleTweetUsingNaiveBayes()
